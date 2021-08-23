8ea1c26546d02b29a00cf09bb58ef42d56a59ba0
Antonio Nardi <antonio.nardi@interacso.com>
2021-08-23 10:51:42 -0400

    Fix services padding section

--- src/components/LazyCard.js
+++ src/components/LazyCard.js
@@ -13,7 +13,7 @@ const LazyCard = (props) => {
   return (
     <Card style={{width: 350, margin: '0 auto', borderRadius: 12, background: props.background}}>
       <CardHeader style={{height: 300}}>
-        <img className="card-img" style={{height: 250, paddingTop: 10}} src={require(`./../assets/img/${props.image}`)} alt="alt desc" />
+        <img className="card-img" style={{height: 250, paddingTop: 10, pointerEvents: 'none'}} src={require(`./../assets/img/${props.image}`)} alt="alt desc" />
       </CardHeader>
       <CardBody>
         <Badge className="mb-2" color={props.tag.color} style={{userSelect: 'none'}}>{props.tag.name}</Badge>
