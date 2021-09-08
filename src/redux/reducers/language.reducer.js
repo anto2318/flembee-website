28c74476f9cf2cd998b5615f098d899af1f5a888
Antonio Nardi <antonio.nardi@interacso.com>
2021-09-07 22:59:30 -0400

    Adding redux

---
+++ src/redux/reducers/language.reducer.js
@@ -0,0 +1,27 @@
+import { LANGUAGE_SUCCESS } from '@constants';
+import es from '@locales/es/messages.json';
+// import { RoutesList, RenderList } from '@routes';
+
+const initialState = {
+    language: 'es',
+    catalogs: { es },
+    routesList: '',//RoutesList('es'),
+    renderList: '', //RenderList('es'),
+};
+
+export const language = (state = initialState, action = {}) => {
+    switch (action.type) {
+        case LANGUAGE_SUCCESS:
+            const { language, catalog } = action.payload;
+            return {
+                ...state,
+                language: language,
+                catalogs: { [language]: catalog },
+                routesList: RoutesList(language),
+                renderList: RenderList(language),
+            };
+
+        default:
+            return state;
+    }
+};
