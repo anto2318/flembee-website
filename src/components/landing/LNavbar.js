0ae673dbeda0d785842d968a3d101d263edf958c
Antonio Nardi <antonionardi@MacBook-Pro-de-Antonio-2.local>
2022-01-11 23:42:49 -0400

    adding webpack

--- src/components/landing/LNavbar.js
+++ src/components/landing/LNavbar.js
@@ -1,9 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Container,
-  Collapse,
   Navbar,
-  NavbarToggler,
   Nav,
   NavItem,
   Button
@@ -11,51 +9,50 @@ import {
 import { NavLink } from 'react-router-dom';
 import logo from './../../assets/img/logo_black.svg';
 
-class LNavbar extends React.Component {
-    constructor(props) {
-      super(props);
-      this.toggle = this.toggle.bind(this);
-      this.state = {
-        isOpen: false
-      };
-    }
+import { useDispatch } from 'react-redux';
+import {
+  changeLanguageProcess,
+} from '@actions';
 
-    toggle () {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
-    }
+const LNavbar = () => {
 
-    render() {
-      return (
-        <Navbar expand="lg" dark color="white" className="mb-4">
-          <Container>
-            <NavLink to="/" className="navbar-brand d-fles align-items-center text-dark">
-              <img className="mr-2" style={{height: 30}} src={logo} alt="logo" />
-              Flembee
-            </NavLink>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-lg-auto" navbar>
-                <NavItem className="mx-2">
-                  <Button style={{
-                        width: 48,
-                        height: 48,
-                        padding: "6px 0px",
-                        borderRadius: 24,
-                        fontSize: 14,
-                        textAlign: "center",
-                        background: "#FFF",
-                        color: "#000"
-                    }}>EN
-                  </Button>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </Container>
-        </Navbar>
-      );
-    }
+  const dispatch = useDispatch();
+
+  const [userLanguage, setUserLanguage] = useState();
+
+  useEffect(() => {
+  localStorage.setItem("userLanguage", userLanguage ? userLanguage : '')
+  }, [userLanguage])
+
+  const onChangeLanguage = async () => {
+    dispatch(changeLanguageProcess())
+  }
+
+    return (
+      <Navbar expand="lg" dark color="white" className="mb-4">
+        <Container>
+          <NavLink to="/" className="navbar-brand d-fles align-items-center text-dark">
+            <img className="mr-2" style={{height: 30}} src={logo} alt="logo" />
+            Flembee
+          </NavLink>
+            <Nav className="ml-lg-auto" navbar>
+              <NavItem className="mx-2">
+                <Button style={{
+                      width: 48,
+                      height: 48,
+                      padding: "6px 0px",
+                      borderRadius: 24,
+                      fontSize: 14,
+                      textAlign: "center",
+                      background: "#FFF",
+                      color: "#000"
+                  }} onClick={() => onChangeLanguage()}>EN
+                </Button>
+              </NavItem>
+            </Nav>
+        </Container>
+      </Navbar>
+    );
 };
 
 export default LNavbar;
--- src/redux/middlewares/language.middleware.js
+++ src/redux/middlewares/language.middleware.js
@@ -3,12 +3,14 @@ import { LANGUAGE_SUCCESS, LANGUAGE_PROCESS } from '@constants';
 export const languageProcess = ({ dispatch }) => (next) => (action) => {
     next(action);
     if (action.type === LANGUAGE_PROCESS) {
+        console.log("PROCESS")
     }
 };
 
 export const updateLanguageSuccess = ({ dispatch }) => (next) => (action) => {
     next(action);
     if (action.type === LANGUAGE_SUCCESS) {
+        console.log("SUCCESS")
     }
 };
 
